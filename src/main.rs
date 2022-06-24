9d171cac2cdb339675c31179bd71eb2029414571
Blake Abel <blakecabel@gmail.com>
2022-06-23 22:49:06 -0400

    Add proper argument handling from the command line

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
 use praeses_blackjack::blackjack;
 
+use clap::Parser;
+
+/// Program to play Blackjack
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Number of real players in the game
+    #[clap(short = 'h', long, value_parser, default_value_t = 1)]
+    human_players: u32,
+
+    /// Number of bot players in the game
+    #[clap(short = 'b', long, value_parser, default_value_t = 0)]
+    bot_players: u32,
+
+    /// Number of decks to use in the game
+    #[clap(short = 'd', long, value_parser, default_value_t = 6)]
+    num_decks: u32,
+
+    /// Payout ratio for the game
+    #[clap(short, long, value_parser, default_value_t = 3.0/2.0)]
+    payout_ratio: f64,
+}
+
 fn main() {
+    let args = Args::parse();
+
     let options = blackjack::GameOptions {
-        num_players: 1,
-        num_decks: 6,
-        payout_ratio: 1.5,
+        num_players: args.human_players,
+        num_decks: args.num_decks,
+        payout_ratio: args.payout_ratio,
     };
 
     blackjack::play_blackjack::<blackjack::player::Dealer>(options);
